a3d4bbbd82a8816e04a1eb54b10508606a43a3a5
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-05-23 00:08:18 +0500

    Создали модели

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    phone_number = models.CharField(max_length=11)
+    operator_code = models.CharField(max_length=3)
+    tag = models.CharField(max_length=255)
+    timezone = models.CharField(max_length=255)
+
+
+class MailingList(models.Model):
+    start_time = models.DateTimeField()
+    message = models.TextField()
+    filter_operator_code = models.CharField(max_length=3)
+    filter_tag = models.CharField(max_length=255)
+    end_time = models.DateTimeField()
+
+
+class Message(models.Model):
+    created_time = models.DateTimeField()
+    status = models.CharField(max_length=255)
+    campaign = models.ForeignKey(MailingList, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
