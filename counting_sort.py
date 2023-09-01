e9680f10abc0231c7339ceebd864d3ce785a2975
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-09-01 09:58:00 +0500

    Линейная сложность сортировки обозначает алгоритм сортировки, который имеет временную сложность, пропорциональную количеству элементов в сортируемом массиве (или коллекции). В математической нотации линейная сложность обозначается как O(n), где n - количество элементов.

---
+++ counting_sort.py
@@ -0,0 +1,26 @@
+def counting_sort(arr):
+    max_val = max(arr)
+    min_val = min(arr)
+    range_of_elements = max_val - min_val +1
+    count_arr = [0] * range_of_elements
+    output_arr = [0] * len(arr)
+
+    # Подчет количества вхождения каждого элемента
+    for i in range(len(arr)):
+        count_arr[arr[i] - min_val] +=1
+
+    # Накопление сумм для определения позиции элементов в отсортированном массиве
+    for i in range(1, len(count_arr)):
+        count_arr[i] += count_arr[i -1]
+
+    # Формирование отсортированного массива
+    for i in range(len(arr) - 1, -1, -1):
+        output_arr[count_arr[arr[i] - min_val] -1]= arr[i]
+        count_arr[arr[i] - min_val] -= 1
+
+    return output_arr
+
+# Пример использования
+arr = [4, 2, 2, 8, 3, 3, 1]
+sorted_arr = counting_sort(arr)
+print(sorted_arr)   # Вывод: [1, 2, 2, 3, 3, 4, 8]
