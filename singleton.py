ae2b02cdcc19399e5ae70d7b8eaa8c93a954c027
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-02-14 10:06:47 +0500

    Добавили функционал для подключения

--- singleton.py
+++ singleton.py
@@ -2,11 +2,17 @@ class DBconnect:
     """Реализация алгоритма Singleton"""
     def __new__(cls, *args, **kwargs):
         print("Вызов __new__")
+        return super().__new__(cls)
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, user, password, port):
+        self.user = user
+        self.password = password
+        self.port = port
+
+    def connect(self):
+        print(f"Соединение с БД: {self.user}, {self.password}, {self.port}.")
 
 
-db = DBconnect(1, 2)
-print(db)
+db1 = DBconnect("Ruslan", 12345, 80)
+db2 = DBconnect("Artur", 54321, 81)              
+print(id(db1), id(db2))
