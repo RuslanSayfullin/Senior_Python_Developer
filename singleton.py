4c03caab97c00e232f0085ad1e5764af23b05998
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-02-14 10:10:34 +0500

    Работает

--- singleton.py
+++ singleton.py
@@ -1,8 +1,11 @@
 class DBconnect:
     """Реализация алгоритма Singleton"""
+    __instance = None
+
     def __new__(cls, *args, **kwargs):
-        print("Вызов __new__")
-        return super().__new__(cls)
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
     def __init__(self, user, password, port):
         self.user = user
