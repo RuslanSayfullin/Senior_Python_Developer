e87553d7144aaeaf89c752c0c266bbf59638e4fe
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-04-15 10:14:13 +0500

    объединить словари в Python с помощью метода dict.update()

---
+++ dict_update.py
@@ -0,0 +1,14 @@
+dict_one = {
+    'id': 1,
+    'name': "Ashutosh",
+    'books': ['Python', 'DSA']
+}
+
+dict_two = {
+    'college': 'NSEC',
+    'city': 'Kolcata',
+    'country': 'India'
+}
+
+dict_one.update(dict_two)
+print(dict_one)
